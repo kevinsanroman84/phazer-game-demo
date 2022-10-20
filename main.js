aee4fc76797eb7757329513c7f6004b6a3529a33
kevinsanroman84 <kevinsanroman84@gmail.com>
2022-10-20 09:20:58 -0700

    Updated background pictures to higher quality

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const gameOptions = {
 }
 window.onload = function () {
     const gameConfig = {
-        width: 270,
-        height: 150,
+        width: 1980,
+        height: 1080,
         scene: [bootGame, playGame]
     }
 
@@ -41,16 +41,19 @@ class playGame extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image("background", "./assets/layers/parallax-mountain-bg.png");
-        this.load.image("foreground1", "./assets/layers/parallax-mountain-1.png");
-        this.load.image("foreground2", "./assets/layers/parallax-mountain-2.png");
-        this.load.image("foreground3", "./assets/layers/parallax-mountain-3.png");
-        this.load.image("foreground4", "./assets/layers/parallax-mountain-4.png");
+        this.load.image("background", "./assets/layers/2.png");
+        this.load.image("foreground1", "./assets/layers/3.png");
+        this.load.image("foreground2", "./assets/layers/4.png");
+        this.load.image("foreground3", "./assets/layers/5.png");
+        this.load.image("foreground4", "./assets/layers/6.png");
+        this.load.image("foreground5", "./assets/layers/7.png");
+        this.load.image("foreground6", "./assets/layers/8.png");
 
         this.load.audio("music", "./assets/music/theVileGrove.wav")
     }
 
     create() {
+
         this.bg_1 = this.add.tileSprite(0, 0, game.config.width, game.config.height, "background");
         this.bg_1.setOrigin(0, 0);
         this.bg_1.setScrollFactor(0);
@@ -71,6 +74,15 @@ class playGame extends Phaser.Scene {
         this.bg_5.setOrigin(0, 0);
         this.bg_5.setScrollFactor(0.75);
 
+        this.bg_6 = this.add.tileSprite(0, 0, game.config.width, game.config.height, "foreground5");
+        this.bg_6.setOrigin(0, 0);
+        this.bg_6.setScrollFactor(0.90);
+
+        this.bg_7 = this.add.tileSprite(0, 0, game.config.width, game.config.height, "foreground6");
+        this.bg_7.setOrigin(0, 0);
+        this.bg_7.setScrollFactor(0.90);
+
+
         const music = this.sound.add("music", { loop: true });
         music.play();
     }
@@ -80,6 +92,8 @@ class playGame extends Phaser.Scene {
         this.bg_3.tilePositionX -= 0.1;
         this.bg_4.tilePositionX -= 0.25;
         this.bg_5.tilePositionX -= 0.50;
+        this.bg_6.tilePositionX -= 0.90;
+        this.bg_7.tilePositionX -= 0.90;
     }
 }
 
