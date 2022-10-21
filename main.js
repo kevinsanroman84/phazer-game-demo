b70a60716a2fea9c346a1e0ff67b740488247e93
kevinsanroman84 <kevinsanroman84@gmail.com>
2022-10-21 15:47:33 -0700

    Added prototype touch screen controls

--- main.js
+++ main.js
@@ -32,7 +32,6 @@ class playGame extends Phaser.Scene {
 
     preload() {
 
-        this.load.image("ground", "./assets/layers/platform.png");
         this.load.image("background", "./assets/layers/2.png");
         this.load.image("foreground1", "./assets/layers/3.png");
         this.load.image("foreground2", "./assets/layers/4.png");
@@ -43,12 +42,21 @@ class playGame extends Phaser.Scene {
 
         this.load.spritesheet("blue_witch_run", "./assets/characters/B_witch_run.png", { frameWidth: 32, frameHeight: 48 });
         this.load.spritesheet("blue_witch_idle", "./assets/characters/B_witch_idle.png", { frameWidth: 32, frameHeight: 48 });
+        this.load.spritesheet("blue_witch_charge", "./assets/characters/B_witch_charge.png", { frameWidth: 48, frameHeight: 48 });
+        this.load.spritesheet("blue_witch_attack", "./assets/characters/B_witch_attack.png", { frameWidth: 104, frameHeight: 46 });
 
         this.load.audio("music", "./assets/music/theVileGrove.wav")
+
+        // Touch control memory
+        this.is_holding = {
+            left: false,
+            right: false,
+            direction: false
+        }
     }
 
     create() {
-        this.physics.world.setBounds(0, 0, undefined, 1020, true, false, false, true);
+        this.physics.world.setBounds(0, 0, undefined, 1000, true, false, false, true);
 
         // Add music to scene
         const music = this.sound.add("music", { loop: true });
@@ -90,7 +98,7 @@ class playGame extends Phaser.Scene {
         this.player.setBounce(0.2);
         this.player.setScale(3);
         this.player.setCollideWorldBounds(true);
-        this.player.body.setGravityY(300);
+        this.player.body.setGravityY(600);
 
 
 
@@ -109,7 +117,23 @@ class playGame extends Phaser.Scene {
                 { start: 0, end: 5 }),
             frameRate: 10,
             repeat: -1
-        })
+        });
+
+        this.anims.create({
+            key: "charge",
+            frames: this.anims.generateFrameNumbers("blue_witch_charge",
+                { start: 0, end: 4 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: "attack",
+            frames: this.anims.generateFrameNumbers("blue_witch_attack",
+                { start: 0, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
 
         // Set main camera to follow character
         this.myCam = this.cameras.main;
@@ -120,10 +144,71 @@ class playGame extends Phaser.Scene {
 
     }
 
+    holdLeft() {
+        this.is_holding.left = true;
+        this.is_holding.direction = "left";
+    }
+
+    holdRight() {
+        this.is_holding.right = true;
+        this.is_holding.direction = "right";
+    }
+
+    releaseLeft() {
+        this.is_holding.left = false;
+
+        if (this.is_holding.right) {
+            this.is_holding.direction = "right";
+        }
+        else {
+            this.is_holding.direction = false;
+        }
+    }
+
+    releaseRight() {
+        this.is_holding.right = false;
+
+        if (this.is_holding.left) {
+            this.is_holding.direction = "left";
+        }
+        else {
+            this.is_holding.direction = false;
+        }
+    }
+
     update() {
         if (gameOver) {
             return;
         }
+
+        // // Visual touch zone
+        // let debug = this.add.graphics({ x: 0, y: 0 });
+        // debug.fillStyle("0x000000", 0.01);
+        // debug.fillRect(0, 0, 0.45 * game.config.width, game.config.height);
+        // debug.setScrollFactor(0);
+        // debug.setDepth(2);
+        // // Left touch zone
+        // this.zone_left = this.add.zone(0, 0, 0.45 * game.config.width, game.config.height);
+        // this.zone_left.setOrigin(0, 0);
+        // this.zone_left.setDepth(2);
+        // this.zone_left.setScrollFactor(0);
+        // // Activate left touch zone
+        // this.zone_left.setInteractive();
+        // this.zone_left.on("pointerdown", holdLeft());
+        // this.zone_left.on("pointerup", this.releaseLeft, this);
+        // this.zone_left.on("pointerout", this.releaseLeft, this);
+        // // Right touch zone 
+        // this.zone_right = this.add.zone(game.config.width, 0, 0.45 * game.config.width, game.config.height);
+        // this.zone_right.setOrigin(1, 0);
+        // this.zone_right.setDepth(2000);
+        // this.zone_right.setScrollFactor(0);
+        // // Activate right touch zone
+        // this.zone_right.setInteractive();
+        // this.zone_right.on("pointerdown", this.holdRight, this);
+        // this.zone_right.on("pointerup", this.releaseRight, this);
+        // this.zone_right.on("pointerout", this.releaseRight, this);
+
+        // Set up controls for game
         if (cursors.left.isDown) {
             this.player.setVelocityX(-260);
             this.player.flipX = true;
@@ -134,10 +219,19 @@ class playGame extends Phaser.Scene {
             this.player.flipX = false;
             this.player.anims.play("run", true);
         }
+        else if (cursors.down.isDown) {
+            this.player.anims.play("charge", true);
+        }
         else {
             this.player.setVelocityX(0);
             this.player.anims.play("idle", true);
         }
+
+        if (cursors.up.isDown && this.player.body.bottom === this.physics.world.bounds.height) {
+            this.player.setVelocityY(-600);
+            this.player.anims.play("run", true);
+        }
+
         this.bg_2.tilePositionX = this.myCam.scrollX * .1;
         this.bg_3.tilePositionX = this.myCam.scrollX * .2;
         this.bg_4.tilePositionX = this.myCam.scrollX * .4;
